7bf8e7c1f0c783abfc3a40e1c0bd4ba6c00a9c02
berta <berta.vincze@gmail.com>
2019-03-10 14:09:27 +0100

    Add XMLHandler instance and functions to WebappContextListener

--- src/main/java/com/codecool/web/listener/WebappContextListener.java
+++ src/main/java/com/codecool/web/listener/WebappContextListener.java
@@ -1,19 +1,37 @@
 package com.codecool.web.listener;
 
+import com.codecool.web.service.XMLHandler;
+import org.xml.sax.SAXException;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.text.ParseException;
 
 @WebListener
 public final class WebappContextListener implements ServletContextListener {
 
+    public static final XMLHandler xmlHandler = new XMLHandler();
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("This method is invoked once when the webapp gets deployed.");
+        try {
+            xmlHandler.loadFromFile("tweets.xml");
+        } catch (IOException | SAXException | ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         System.out.println("This method is invoked once when the webapp gets undeployed.");
+        try {
+            xmlHandler.saveToFile("tweets.xml");
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
     }
 }
